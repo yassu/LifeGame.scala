4200fa8e281fb76301fd9f605aa24e46b89b1889
yassu <yassu0320.dev@gmail.com>
2018-06-10 14:51:20 +0900

    Add Block and Blinker Sample Board

---
+++ src/main/scala/SampleBoards.scala
@@ -0,0 +1,19 @@
+package lifegame.sampleboards
+
+import lifegame.elements.{Board, Cell, White, Black}
+
+object SampleBoards {
+  def blockBoard: Board = Board(List(
+    List(Cell(White), Cell(White), Cell(White), Cell(White)),
+    List(Cell(White), Cell(Black), Cell(Black), Cell(White)),
+    List(Cell(White), Cell(Black), Cell(Black), Cell(White)),
+    List(Cell(White), Cell(White), Cell(White), Cell(White))
+  ))
+  def blinkerBoard: Board = Board(List(
+    List(Cell(White), Cell(White), Cell(White), Cell(White), Cell(White)),
+    List(Cell(White), Cell(White), Cell(White), Cell(White), Cell(White)),
+    List(Cell(White), Cell(Black), Cell(Black), Cell(Black), Cell(White)),
+    List(Cell(White), Cell(White), Cell(White), Cell(White), Cell(White)),
+    List(Cell(White), Cell(White), Cell(White), Cell(White), Cell(White)),
+  ))
+}
